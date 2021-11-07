8906919381348d83a5b3ed8a829f8bf19d5414f7
anoop-rc <rc.anoop@gmail.com>
2021-11-07 23:24:51 +0530

    login page update for firebase login error

--- firebase.js
+++ firebase.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth, GoogleAuthProvider } from "@firebase/auth";
+import { initializeApp } from 'firebase/app'
+import { GoogleAuthProvider, getAuth } from 'firebase/auth'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,8 +17,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const provider = new GoogleAuthProvider();
-const auth = getAuth();
+const app = initializeApp(firebaseConfig)
+const provider = new GoogleAuthProvider()
+const auth = getAuth()
 
-export {app, provider, auth}
+export { app, provider, auth }
--- pages/login.js
+++ pages/login.js
@@ -1,31 +1,33 @@
-import {useEffect} from 'react'
-import tw from "tailwind-styled-components"
-import {useRouter} from 'next/router'
-import {signInWithPopup, onAuthStateChanged} from 'firebase/auth'
-import {auth, provider} from '../firebase'
-
+import tw from 'tailwind-styled-components'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { signInWithPopup } from 'firebase/auth'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth, provider } from '../firebase'
 
 const Login = () => {
-    const router = useRouter()
+  const router = useRouter()
 
-    useEffect(()=>{
-        onAuthStateChanged(auth, user=>{
-            if(user){
-                router.push('/')
-            }
-        })
-    },[])
+  useEffect(() => {
+    onAuthStateChanged(auth, user => {
+      if (user) {
+        router.push('/')
+      }
+    })
+  }, [])
 
-    return (
-        <Wrapper>
-            <UberLogo src='https://i.ibb.co/ZMhy8ws/uber-logo.png'></UberLogo>
-            <Title>Log in to access your account</Title>
-            <HeadImage src='https://i.ibb.co/CsV9RYZ/login-image.png'></HeadImage>
-            <SignInButton onClick={()=>signInWithPopup(auth,provider)}>
-                Sign in with Google
-            </SignInButton>
-        </Wrapper>
-    )
+  return (
+    <Wrapper>
+      <UberLogo src='https://i.ibb.co/ZMhy8ws/uber-logo.png' />
+      <TopSection>
+        <Title>Log in to access your account</Title>
+        <HeadImage src='https://i.ibb.co/CsV9RYZ/login-image.png' />
+        <SignInButton onClick={() => signInWithPopup(auth, provider)}>
+          Sign in with Google
+        </SignInButton>
+      </TopSection>
+    </Wrapper>
+  )
 }
 
 export default Login
@@ -39,16 +41,15 @@ h-20 w-auto object-contain self-start
 `
 
 const TopSection = tw.div`
-flex flex-col flex-grow
+flex  flex-col flex-grow
 `
+
 const Title = tw.div`
 text-5xl pt-4 text-gray-500
 `
-
 const HeadImage = tw.img`
 object-contain w-full
 `
-
 const SignInButton = tw.button`
 bg-black text-white text-center py-4 mt-8 self-center w-full cursor-pointer
 `
